7511966bbb2f9e13810059e1191d41b41251b544
Mohammad Muzaffaruddin <muzaffarm7@gmail.com>
2020-08-09 13:15:20 +0530

    commited

--- EDUTECH/settings.py
+++ EDUTECH/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = True
 
 # Set hosts to allow any app on Heroku and the local testing URL
-ALLOWED_HOSTS = ['EDUTECH_App.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['ell555.herokuapp.com','EDUTECH_App.herokuapp.com','127.0.0.1']
 
 
 # Application definition
