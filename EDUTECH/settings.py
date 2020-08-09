312a7b5525e7b4088447ca575119c2188adc3ae4
Mohammad Muzaffaruddin <muzaffarm7@gmail.com>
2020-08-09 13:31:04 +0530

    commited

--- EDUTECH/settings.py
+++ EDUTECH/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ii4wc&cz)!=ivd4vmahzbqaj@7t3@0*v#5wd5v#*)nh2ge3mo5'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -124,6 +121,15 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+
+# Add to test email:
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
@@ -134,10 +140,6 @@ STATIC_URL = '/static/'
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# The URL to use when referring to static files (where they will be served from)
-STATIC_URL = '/static/'
-
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
